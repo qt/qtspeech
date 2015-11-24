8eaad290524dafb104b6c5ed4f9756950d4fb776
Maurice Kalinowski <maurice.kalinowski@theqtcompany.com>
2015-11-24 14:26:38 +0100

    Add private header comment section
    Build system otherwise complains about a missing "we mean it" section.
    
    Change-Id: I84d38bb5dceeb3ba4acbb5572d6efab28071c14c
    Reviewed-by: Jeremy Whiting <jpwhiting@kde.org>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@theqtcompany.com>

--- src/tts/qvoice_p.h
+++ src/tts/qvoice_p.h
@@ -39,6 +39,17 @@
 #ifndef QVOICE_P_H
 #define QVOICE_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <qvoice.h>
 
 #include <QString>
