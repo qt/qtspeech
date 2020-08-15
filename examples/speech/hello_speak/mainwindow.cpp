aec9bb44af76b5e63afec151a36d057fa738aca0
Paul Wicking <paul.wicking@qt.io>
2020-08-15 11:14:06 +0200

    Use nullptr instead of 0 in example
    Fixes: QTBUG-86059
    Pick-to: 5.15
    Change-Id: Ifffad0a0767387c92325143a94700d8b5bff5600
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- examples/speech/hello_speak/mainwindow.cpp
+++ examples/speech/hello_speak/mainwindow.cpp
@@ -55,7 +55,7 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
-    m_speech(0)
+    m_speech(nullptr)
 {
     ui.setupUi(this);
     QLoggingCategory::setFilterRules(QStringLiteral("qt.speech.tts=true \n qt.speech.tts.*=true"));
