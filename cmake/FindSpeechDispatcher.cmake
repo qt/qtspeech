2a83ceee18fb309a7d743b17df849375a54ad9dd
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-03-23 14:57:40 +0100

    Fix build with SpeechDispatcher
    Change-Id: Ie59d3db34ca37ca628ace6860b400ed7827a2e00
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/FindSpeechDispatcher.cmake
+++ cmake/FindSpeechDispatcher.cmake
@@ -1,4 +1,11 @@
-include(FindPkgConfig)
+# We can't create the same interface imported target multiple times, CMake will complain if we do
+# that. This can happen if the find_package call is done in multiple different subdirectories.
+if(TARGET SpeechDispatcher::SpeechDispatcher)
+    set(SpeechDispatcher_FOUND 1)
+    return()
+endif()
+
+find_package(PkgConfig QUIET)
 
 pkg_check_modules(SpeechDispatcher "speech-dispatcher" IMPORTED_TARGET GLOBAL)
 
