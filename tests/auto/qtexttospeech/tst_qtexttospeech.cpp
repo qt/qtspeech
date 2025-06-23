9510edd94049cbb91882b0dd2e117a48bf2811fe
Tim Blechmann <tim.blechmann@qt.io>
2025-06-23 14:37:51 +0800

    Tests: skip sayingWordWithPause for flite engine
    `QTextToSpeech::pause(BoundaryHint::Word)` is not implemented on the
    flite engine.
    
    Task-number: QTBUG-137947
    Task-number: QTBUG-137735
    Pick-to: 6.8 6.9 6.10
    Change-Id: Idd6982ab8e89df77514f5ca59b724286c9b5e3fa
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tests/auto/qtexttospeech/tst_qtexttospeech.cpp
+++ tests/auto/qtexttospeech/tst_qtexttospeech.cpp
@@ -877,6 +877,10 @@ void tst_QTextToSpeech::sayingWordWithPause()
     if (engine != "mock" && !hasDefaultAudioOutput())
         QSKIP("No audio device present");
 
+    if (engine == "flite")
+        QSKIP("QTBUG-137947 QTextToSpeech::pause(QTextToSpeech::BoundaryHint::Word) not implemented"
+              " for flite");
+
     QFETCH(QStringList, words);
     QFETCH(int, pauseAt);
 
